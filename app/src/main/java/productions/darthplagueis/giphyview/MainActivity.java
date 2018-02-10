88fbf745d2c2bb83876550569ef87211075b3500
osibo00 <olegsiboglov@ac.c4q.nyc>
2018-02-10 03:49:19 -0500

    added gif download

--- app/src/main/java/productions/darthplagueis/giphyview/MainActivity.java
+++ app/src/main/java/productions/darthplagueis/giphyview/MainActivity.java
@@ -1,22 +1,35 @@
 package productions.darthplagueis.giphyview;
 
+import android.Manifest;
+import android.app.DownloadManager;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.util.List;
@@ -39,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     private GiphyAdapter adapter;
     private GifViewModel gifViewModel;
     private boolean hasExecutedInitialCall;
-    private boolean updateWithDiff;
+    private String[] gifDownloadData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         preferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
         hasExecutedInitialCall = preferences.getBoolean("has_executed", false);
 
-        //DatabaseInitializer.removeAllFromDb(GifDatabase.getDatabase(getApplicationContext()));
+        //DatabaseInitializer.removeAllFromDb(GifDatabase.getDatabase(getApplicationContext()));//
 
         giphyRecyclerView = findViewById(R.id.giphy_recyclerview);
         adapter = new GiphyAdapter();
@@ -60,12 +73,12 @@ public class MainActivity extends AppCompatActivity {
 
         gifViewModel = ViewModelProviders.of(this).get(GifViewModel.class);
 
-        createJobService();
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, new IntentFilter("gif-download"));
+
         if (!hasExecutedInitialCall) {
-            //createJobService();
+            createJobService();
             if (isNetworkAvailable()) {
                 GiphyRetrofit.makeApiCall(getApplicationContext(), false);
-                makeSnackbar("Swipe left or right to remove Gifs");
             } else {
                 Toast.makeText(MainActivity.this, "Please check your internet connection" +
                         " and try again", Toast.LENGTH_LONG).show();
@@ -74,10 +87,29 @@ public class MainActivity extends AppCompatActivity {
             showGifsInUi();
             makeSnackbar("Swipe left or right to remove Gifs");
         }
-
         onPostExecuteListener();
         setSwipeListener();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            gifDownload();
+        }
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null) {
+                gifDownloadData = intent.getExtras().getStringArray("gif-extras");
+            }
+            if (haveStoragePermission()) {
+                gifDownload();
+            }
+        }
+    };
 
     private void createJobService() {
         long ONE_DAY_INTERVAL = 24 * 60 * 60 * 1000L;
@@ -88,14 +120,20 @@ public class MainActivity extends AppCompatActivity {
                 .build());
     }
 
+    private boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
     private void onPostExecuteListener() {
-        DatabaseInitializer.setAdapterResponse(new DatabaseInitializer.AdapterResponse() {
+        DatabaseInitializer.setCallResponse(new DatabaseInitializer.CallResponse() {
             @Override
             public void onPostExecute() {
                 hasExecutedInitialCall = true;
                 preferences.edit().putBoolean("has_executed", hasExecutedInitialCall).apply();
-                updateWithDiff = true;
                 showGifsInUi();
+                makeSnackbar("Swipe left or right to remove Gifs");
             }
         });
     }
@@ -105,17 +143,37 @@ public class MainActivity extends AppCompatActivity {
         gifViewModel.getGifsList().observe(this, new Observer<List<GiphyGif>>() {
             @Override
             public void onChanged(@Nullable List<GiphyGif> gifList) {
-                updateRecyclerView(gifList);
-                if (updateWithDiff) {
-                    adapter.updateWithDifference(gifList);
-                    updateWithDiff = false;
-                }
+                adapter.updateWithDifference(gifList);
             }
         });
     }
 
-    private void updateRecyclerView(List<GiphyGif> gifList) {
-        adapter.passListToAdapter(gifList);
+    private boolean haveStoragePermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission: " + "You have permission");
+                return true;
+            } else {
+                Log.d(TAG, "Permission: " + "You have asked for permission");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else {
+            Log.d(TAG, "Permission: " + "You already have the permission");
+            return true;
+        }
+    }
+
+    private void gifDownload() {
+        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(gifDownloadData[0]));
+        String downloadTitle = "GiphyView " + String.valueOf(gifDownloadData[2]) + ".mp4";
+        request.setTitle(gifDownloadData[1])
+                .setDescription("Downloading Gif")
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, downloadTitle);
+        downloadManager.enqueue(request);
     }
 
     private void setSwipeListener() {
@@ -144,12 +202,6 @@ public class MainActivity extends AppCompatActivity {
         };
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(itemCallBack);
         itemTouchHelper.attachToRecyclerView(giphyRecyclerView);
-    }
-
-    private boolean isNetworkAvailable() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        return networkInfo != null && networkInfo.isConnected();
     }
 
     private void makeSnackbar(String text) {
